c9f0457387e8b6de580aae2f0b5c23d130c06090
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-11-23 22:35:35 +0500

    colors

--- app.js
+++ app.js
@@ -14,11 +14,21 @@ function generateRandomColor() {
 function setRandomColors() {
   cols.forEach((col) => {
     const text = col.querySelector('h2')
+    const button = col.querySelector('button')
     const color = chroma.random()
 
     text.textContent = color
     col.style.background = color
+
+    setTextColor(text, color)
+    setTextColor(button, color)
+    console.log('123')
   })
+}
+
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance()
+  text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
 setRandomColors()
